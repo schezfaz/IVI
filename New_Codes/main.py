cbece11cf5e4c93ccc14ecfdef227eb961fef23b
bhavyameghnani <meghnani.bhavya@gmail.com>
2021-06-13 22:57:35 +0530

    Backend Snippet Updated

--- New_Codes/main.py
+++
@@ -1,9 +0,0 @@
-import os
-# set the paths
-pathToScript = 'pdf2txt.py'
-pathPDFinput = 'ivi.pdf'
-pathHTMLoutput = 'test.html'
-
-# call the pdf2txt.py from the command line
-os.system('python {} -o {} -S {} -t html'.format(pathToScript, pathHTMLoutput, pathPDFinput))
-  
